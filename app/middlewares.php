04749c7d6cbcbc818c416b1efab8d766b745195a
Fery Wardiyanto <ferywardiyanto@gmail.com>
2015-11-25 17:44:13 +0700

    Add Basic Authentication middleware & Content Negotiation provider
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- app/middlewares.php
+++ app/middlewares.php
@@ -12,17 +12,8 @@ Middleware::setStreamFactory(function () use ($container) {
     return $container->get('request')->getBody();
 });
 
-$app->add(new Middleware\Robots());
-$app->add(new Middleware\ResponseTime());
-$app->add(new Middleware\TrailingSlash(true));
-// $app->add(new Middleware\FormatNegotiator());
-// $app->add(new Middleware\LanguageNegotiator(['id', 'en']));
-
-// $app->add(
-//     Middleware::When()
-//         ->condition($settings['mode'] === 'development')
-//         ->middleware(Middleware::debugBar()->from($container, 'debugbar'))
-// );
+$app->add(Middleware::TrailingSlash(true));
+$app->add(Middleware::FormatNegotiator());
 
 /**
  * Log every request
@@ -33,4 +24,67 @@ $app->add(function (Request $req, Response $res, Callable $next) {
     $this->get('logger')->debug($req->getMethod().' '.$req->getUri()->getPath());
 
     return $res;
+});
+
+/**
+ * Middleware to add or remove the trailing slash.
+ */
+$app->add(function (Request $req, Response $res, Callable $next) {
+    $uri = $req->getUri();
+    $path = $uri->getPath();
+    $is_true = true;
+
+    if ($is_true) {
+        if (strlen($path) > 1 && substr($path, -1) !== '/' && !pathinfo($path, PATHINFO_EXTENSION)) {
+            $path .= '/';
+        }
+    } else {
+        if (strlen($path) > 1 && substr($path, -1) === '/') {
+            $path = substr($path, 0, -1);
+        }
+    }
+
+    //Ensure the path has one "/"
+    if (empty($path) || $path === $this->basePath) {
+        $path .= '/';
+    }
+
+    //redirect
+    if (is_int($this->redirectStatus) && ($uri->getPath() !== $path)) {
+        return self::getRedirectResponse($this->redirectStatus, $uri->withPath($path), $res);
+    }
+
+    return $next($req->withUri($uri->withPath($path)), $res);
+});
+
+/**
+ * Middleware to calculate the response time duration.
+ */
+$app->add(function (Request $req, Response $res, Callable $next) {
+    $server = $req->getServerParams();
+
+    if (!isset($server['REQUEST_TIME_FLOAT'])) {
+        $server['REQUEST_TIME_FLOAT'] = microtime(true);
+    }
+
+    $res = $next($req, $res);
+    $time = (microtime(true) - $server['REQUEST_TIME_FLOAT']) * 1000;
+
+    return $res->withHeader('X-Response-Time', sprintf('%2.3fms', $time));
+});
+
+/**
+ * Middleware to block robots search engine.
+ */
+$app->add(function (Request $req, Response $res, Callable $next) {
+    if ($req->getUri()->getPath() === '/robots.txt') {
+        $body = new Body(fopen('php://temp', 'r+'));
+        $body->write("User-Agent: *\nDisallow: /");
+
+        return $res->withBody($body);
+    }
+
+    $res = $next($req, $res);
+
+    return $res->withHeader('X-Robots-Tag', 'noindex, nofollow, noarchive');
 });
---
+++ app/src/Providers/NegotiatorProvider.php
@@ -0,0 +1,120 @@
+<?php
+namespace App\Providers;
+
+use Negotiation;
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class NegotiatorProvider implements ServiceProviderInterface
+{
+    /**
+     * @var string Default format
+     */
+    protected $default = [
+        'format' => 'html',
+        'language' => 'en',
+    ];
+
+    /**
+     * @var array Available formats with the mime types
+     */
+    protected $formats = [
+        'html'  => ['text/html', 'application/xhtml+xml'],
+        'css'   => ['text/css'],
+        'gif'   => ['image/gif'],
+        'png'   => ['image/png', 'image/x-png'],
+        'jpg'   => ['image/jpeg', 'image/jpg'],
+        'jpeg'  => ['image/jpeg', 'image/jpg'],
+        'json'  => ['application/json', 'text/json', 'application/x-json'],
+        'jsonp' => ['text/javascript', 'application/javascript', 'application/x-javascript'],
+        'js'    => ['text/javascript', 'application/javascript', 'application/x-javascript'],
+        'pdf'   => ['application/pdf', 'application/x-download'],
+        'rdf'   => ['application/rdf+xml'],
+        'rss'   => ['application/rss+xml'],
+        'atom'  => ['application/atom+xml'],
+        'xml'   => ['text/xml', 'application/xml', 'application/x-xml'],
+        'txt'   => ['text/plain'],
+        'mp4'   => ['video/mp4'],
+        'ogg'   => ['audio/ogg'],
+        'ogv'   => ['video/ogg'],
+        'webm'  => ['video/webm'],
+        'webp'  => ['image/webp'],
+        'svg'   => ['image/svg+xml'],
+        'zip'   => ['application/zip', 'application/x-zip', 'application/x-zip-compressed'],
+    ];
+
+    /**
+     * Register this content negotiator provider with a Pimple container
+     *
+     * @param \Pimple\Container $container
+     */
+    public function register(Container $container)
+    {
+        $container['negotiator'] = $this;
+    }
+
+    /**
+     * Get content request format
+     *
+     * @param  Psr\Http\Message\ServerRequestInterface $request
+     * @return string
+     */
+    public function getFormat(ServerRequestInterface $request)
+    {
+        $format = strtolower(pathinfo($request->getUri()->getPath(), PATHINFO_EXTENSION));
+
+        if (isset($this->formats[$format])) {
+            return $format;
+        }
+
+        $accept = $request->getHeaderLine('Accept');
+        if (empty($accept)) {
+            return $this->default['format'];
+        }
+
+        $priorities = call_user_func_array('array_merge', array_values($this->formats));
+
+        try {
+            $accept = (new Negotiation\Negotiator())->getBest($accept, $priorities);
+        } catch (\Exception $exception) {
+            return $this->default['format'];
+        }
+
+        if ($accept) {
+            $accept = $accept->getValue();
+
+            foreach ($this->formats as $extension => $headers) {
+                if (in_array($accept, $headers)) {
+                    return $extension;
+                }
+            }
+        }
+
+        return $this->default['format'];
+    }
+
+    /**
+     * Get content language
+     *
+     * @param  Psr\Http\Message\ServerRequestInterface $request
+     * @return string
+     */
+    public function getLanguage(ServerRequestInterface $request)
+    {
+        $accept = $request->getHeaderLine('Accept-Language');
+        $priorities = ['id', 'en'];
+
+        try {
+            $accept = (new Negotiation\LanguageNegotiator())->getBest($accept, $priorities);
+        } catch (\Exception $exception) {
+            return $this->default['language'];
+        }
+
+        if ($accept) {
+            return $accept->getValue();
+        }
+
+        return $this->default['language'];
+    }
+}

701d80dea4f9551f9d386fe1367895f925661917
Fery Wardiyanto <ferywardiyanto@gmail.com>
2016-06-26 00:25:02 +0700

    Update app and test codebase
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
-define('ROOT_DIR',  dirname(__DIR__).'/');
 define('APP_DIR',   __DIR__.'/');
+define('ROOT_DIR',  dirname(APP_DIR).'/');
 define('ASSET_DIR', ROOT_DIR.'asset/');
 define('WWW_DIR',   ROOT_DIR.'public/');
 
@@ -14,5 +14,5 @@ if (file_exists(ROOT_DIR.'.env')) {
 
 // Initialize Slim\App
 return new Slim\App([
-    'settings' => require_once APP_DIR.'settings.php'
+    'settings' => require_once __DIR__.'/settings.php'
 ]);

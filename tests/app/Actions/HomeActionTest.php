d0cb2cb718063e696955ad819f93713de42c446d
Fery Wardiyanto <ferywardiyanto@gmail.com>
2016-08-16 20:58:08 +0700

    Fix testing issue
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- tests/app/Actions/HomeActionTest.php
+++ tests/app/Actions/HomeActionTest.php
@@ -1,10 +1,12 @@
 <?php
 namespace App\Tests\Actions;
 
+use App\Controllers\HomeController;
+
 class HomeActionTest extends \PHPUnit_Framework_TestCase
 {
     public function testHomeActionIsExists()
     {
-        $this->assertTrue(class_exists('App\Actions\HomeAction'));
+        $this->assertTrue(class_exists(HomeController::class));
     }
 }

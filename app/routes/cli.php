b05add896b527501b4f8a606d4a5baedf7f6d6b5
Fery Wardiyanto <ferywardiyanto@gmail.com>
2016-10-05 01:53:32 +0700

    Update
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- app/routes/cli.php
+++ app/routes/cli.php
@@ -11,4 +11,3 @@ use Projek\Slim\Commands\MigrateCommand;
 $app->get('/', DefaultCommand::class)->setName('default-command');
 
 $app->get('/migrate[/{action}]', MigrateCommand::class)->setName('migration-command');
-
